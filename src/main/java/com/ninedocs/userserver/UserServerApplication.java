0decef64574971ea8a4ad1489c0b5f54ead17765
gunwoda <95297035+gunwoda@users.noreply.github.com>
2024-12-31 11:31:37 +0900

    feat: 스웨거 연동 (#1)

--- src/main/java/com/ninedocs/userserver/UserServerApplication.java
+++ src/main/java/com/ninedocs/userserver/UserServerApplication.java
@@ -1,4 +1,4 @@
-package com.gunwoda.userserver;
+package com.ninedocs.userserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserServerApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(UserServerApplication.class, args);
+  }
 }
