3a06aae6943008789bfc54a67f303a08b6114277
yelimkim98 <kiel0103@naver.com>
2025-01-14 12:08:55 +0900

    feat: user profile API endpoint 수정

--- src/main/java/com/ninedocs/userserver/user/application/userprofile/dto/UserProfileRequest.java
+++
@@ -1,9 +0,0 @@
-package com.ninedocs.userserver.user.application.userprofile.dto;
-
-import lombok.Data;
-
-@Data
-public class UserProfileRequest {
-
-  private long id;
-}
--- src/main/java/com/ninedocs/userserver/user/presentation/UserProfileController.java
+++ src/main/java/com/ninedocs/userserver/user/presentation/UserProfileController.java
@@ -2,13 +2,13 @@ package com.ninedocs.userserver.user.presentation;
 
 import com.ninedocs.userserver.common.presentation.dto.ApiResponse;
 import com.ninedocs.userserver.user.application.userprofile.UserProfileService;
-import com.ninedocs.userserver.user.application.userprofile.dto.UserProfileRequest;
 import com.ninedocs.userserver.user.application.userprofile.dto.UserProfileResponse;
 import com.ninedocs.userserver.user.persistence.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,11 +18,12 @@ public class UserProfileController {
   private final UserProfileService userProfileService;
   private final UserRepository userRepository;
 
-  @PostMapping("/api/v1/user/check")
+  @GetMapping("/api/v1/user/{userId}")
   public ResponseEntity<ApiResponse<UserProfileResponse>> checkUser(
-      UserProfileRequest userProfileRequest) {
-    UserProfileResponse userProfileResponse = userProfileService.getUserProfile(
-        userProfileRequest.getId());
-    return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(userProfileResponse));
+      @PathVariable Long userId
+  ) {
+    return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(
+        userProfileService.getUserProfile(userId)
+    ));
   }
 }
