66e248255f64a7d7115cec03872b3f8d45533cb1
yerimkim <kiel0103@naver.com>
2025-01-30 11:25:00 +0900

    feat: default timezone 설정

--- src/main/java/com/ninedocs/userserver/UserServerApplication.java
+++ src/main/java/com/ninedocs/userserver/UserServerApplication.java
@@ -1,5 +1,7 @@
 package com.ninedocs.userserver;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -9,5 +11,10 @@ public class UserServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(UserServerApplication.class, args);
+  }
+
+  @PostConstruct
+  public void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
   }
 }
