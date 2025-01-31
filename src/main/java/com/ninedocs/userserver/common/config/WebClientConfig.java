43ff264df0b04116403509714514481bff96c5bc
gunwoda <95297035+gunwoda@users.noreply.github.com>
2025-01-31 10:53:07 +0900

    refactor: email url configuration 설정 (#24)
    * refactor: email url configuration 설정
    
    * refactor: Dockerfile 수정

--- src/main/java/com/ninedocs/userserver/common/config/WebClientConfig.java
+++ src/main/java/com/ninedocs/userserver/common/config/WebClientConfig.java
@@ -1,16 +1,23 @@
 package com.ninedocs.userserver.common.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@EnableConfigurationProperties(EmailWebClientProperties.class)
+@RequiredArgsConstructor
 public class WebClientConfig {
 
+  private final WebClient.Builder webClient;
+
   @Bean
-  public WebClient webClient(WebClient.Builder builder) {
-    return builder
-        .baseUrl("http://15.165.213.49:3000")
+  public WebClient webClient(EmailWebClientProperties properties) {
+    return webClient
+        .baseUrl(properties.getUrl())
         .defaultHeader("Content-Type", "application/json")
         .build();
   }
