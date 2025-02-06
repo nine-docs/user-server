59e759174c4ff300549d8069566d5789917f6abb
Kim Yerim <kiel0103@naver.com>
2025-02-06 09:36:12 +0900

    feat: user profile bulk 조회 (#29)

--- src/main/java/com/ninedocs/userserver/user/persistence/UserRepository.java
+++ src/main/java/com/ninedocs/userserver/user/persistence/UserRepository.java
@@ -1,12 +1,18 @@
 package com.ninedocs.userserver.user.persistence;
 
 
+import io.lettuce.core.dynamic.annotation.Param;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByIdAndDeletedAtIsNull(long id);
+
+  @Query("select u from User u where u.id in :userIds and u.deletedAt is null")
+  List<User> findUsers(@Param("userIds") List<Long> userIds);
 
   Optional<User> findByEmailAndDeletedAtIsNull(String email);
 
