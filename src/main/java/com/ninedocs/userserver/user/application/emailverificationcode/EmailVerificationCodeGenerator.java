dc4367a08984bce343ed230b5dfe99e1f371b30f
gunwoda <95297035+gunwoda@users.noreply.github.com>
2025-02-21 12:18:18 +0900

    feat: 인증번호 랜덤생성기 주석처리 해제 (#41)
    Co-authored-by: yelimkim98 <kiel0103@naver.com>

--- src/main/java/com/ninedocs/userserver/user/application/emailverificationcode/EmailVerificationCodeGenerator.java
+++ src/main/java/com/ninedocs/userserver/user/application/emailverificationcode/EmailVerificationCodeGenerator.java
@@ -1,9 +1,6 @@
 package com.ninedocs.userserver.user.application.emailverificationcode;
 
-import com.ninedocs.userserver.user.persistence.UserRepository;
-import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +9,5 @@ public class EmailVerificationCodeGenerator {
 
   public String generateVerificationCode() {
     return String.valueOf((int) (Math.random() * 900000) + 100000); // 6자리 랜덤 숫자
-//    return "111111";
   }
 }
